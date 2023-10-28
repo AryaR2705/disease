c59f82d710932ee3ffdd8d623f76710641562f91
AryaR2705 <139691040+AryaR2705@users.noreply.github.com>
2023-10-28 17:01:59 +0530

    web.py

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/predict": {"origins": "http://localhost:5173"}})
 
 # Load the trained model
-model = joblib.load('disease_prediction_model.pkl')
+model = joblib.load('yessssss.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
