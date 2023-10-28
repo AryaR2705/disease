3bd922d6aa464551c23d48b537fafc5b840fe52a
Arya Ramteke <aryaramteke@Aryas-MacBook-Air.local>
2023-10-28 16:19:43 +0530

    first commit

---
+++ web.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS from flask_cors
+import joblib
+
+app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": "http://localhost:5173"}})
+
+# Load the trained model
+model = joblib.load('disease_prediction_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    predictions = model.predict(data)
+    return jsonify(predictions.tolist())
+
+if __name__ == '__main__':
+    app.run(debug=True)
